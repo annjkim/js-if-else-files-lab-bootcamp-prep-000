77037bf51b7303a7b7956520aa0151ce50c44cc3
annkim822@gmail.com Kim <annkim822@gmail.com>
2018-05-01 15:52:06 +0000

    automatically backed up by learn

--- tests.js
+++ tests.js
@@ -6,6 +6,7 @@ if (index.name === "Susan") {
     console.log("Expected: Susan, Received: "+index.name)
 }
 
+var height = 70
 if (index.height === 70) {
     console.log("The height is correct")
 } else {
